0009bdf716c977828156b97539fd7734293573c4
Priya2811005 <plimbasiya58@gmail.com>
2025-07-22 11:17:43 +0530

    new

---
+++ T15.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        int originalNumber = number;
+        int result = 0;
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+            result += digit * digit * digit;
+            number /= 10;
+        }
+
+
+        if (result == originalNumber)
+        {
+            Console.WriteLine(originalNumber + " is an Armstrong number.");
+        }
+        else
+        {
+            Console.WriteLine(originalNumber + " is not an Armstrong number.");
+        }
+    }
+}
+
+
