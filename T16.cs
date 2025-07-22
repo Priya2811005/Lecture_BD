f99d6c1ee26989ca7ed0d70df6f4fc410e6a0856
Priya2811005 <plimbasiya58@gmail.com>
2025-07-22 11:09:12 +0530

    Initial commit - added Visual Studio project

---
+++ T16.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter number of rows: ");
+        int rows = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine(); 
+        }
+    }
+}
